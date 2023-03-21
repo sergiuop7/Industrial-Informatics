53ebd77105d93762ef87212ee55d670516f855a4
Oprea Sergiu-Daniel <84630418+sergiuop7@users.noreply.github.com>
2023-03-21 21:21:45 +0200

    Add files via upload

---
+++ Lab/Lab1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x = 3, a = 0, b = 1;
+            Console.WriteLine("Enter n the number of Fibonacci terms:");
+            n = int.Parse(Console.ReadLine());
+            if (n == 0)
+                Console.WriteLine("No Fibonacci terms");
+            else if (n == 1)
+                Console.WriteLine(a);
+            else if (n == 2)
+                Console.WriteLine("{0}, {1}", a, b);
+            else
+            {
+                Console.Write("{0}, {1}", a, b);
+                while (x <= n)
+                {
+                    int c = a + b;
+                    a = b; b = c;
+                    Console.Write(", " + c);
+                    x++;
+                }
+            }
+        }
+    }
+}
